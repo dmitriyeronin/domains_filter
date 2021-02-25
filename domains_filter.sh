80e73cb7aee0f2b0e481b693fc24fbd036e6000b
dmitriyeronin <79090019+dmitriyeronin@users.noreply.github.com>
2021-02-25 11:38:11 +0300

    Update domains_filter.sh

--- domains_filter.sh
+++ domains_filter.sh
@@ -4,11 +4,11 @@ if [[ -z "$1" || -z "$2" ]]; then echo "usage: $0 <pcap file> <txt file>"; exit 
 if [ ! -e "$1" ]; then echo "$0: $1: file does not exist"; exit 1; fi
 if [ ! -e "$2" ]; then echo "$0: $2: file does not exist"; exit 1; fi
 
-cat /dev/null > output_domains.txt
 input_domains="$2"
 for domain in $(cat $input_domains)
 do
 	if [ -z "$domains" ]; then domains="dns.qry.name == $domain";
 	else domains="$domains || dns.qry.name == $domain"; fi
 done
-tshark -r output.pcap -Y "$domains" -T fields -e dns.qry.name | uniq
+tshark -r output.pcap -Y "$domains" -T fields -e dns.qry.name | uniq > output_domains.txt
+
